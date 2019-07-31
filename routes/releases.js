c682a87b7990c37c70030e7d0ed4289c59d61d16
Chathura Hettiarachchi <Chathura.h@fidenz.com>
2019-07-31 17:17:07 +0530

    added release get call

--- routes/releases.js
+++ routes/releases.js
@@ -38,6 +38,41 @@ router.get('/', (req, res, next) => {
         })
 });
 
+/* GET releases listing. */
+router.get('/all', (req, res, next) => {
+    const client = new Client();
+    client.connect()
+        .then(() => {
+            const sql = "SELECT r.release_id, r.description, r.quotation_id, r.release_date, q.created_at AS quotation_created_at, q.title AS quotation_title, cs.name AS client_name, cs.code AS client_code FROM release_plans r INNER JOIN quotations q ON r.quotation_id=q.quotation_id INNER JOIN clients cs ON q.client_id=cs.client_id";
+            return client.query(sql);
+        })
+        .then(result => {
+            if (result.rows.length === 0) {
+                res.status(200).json({
+                    status: 1,
+                    message: 'No release found'
+                });
+            } else {
+                res.status(200).json({
+                    status: 1,
+                    message: 'Available releases',
+                    content: {
+                        releases: result.rows
+                    }
+                });
+            }
+        })
+        .catch(e => {
+            res.status(400).json({
+                status: 0,
+                message: 'Something went wrong',
+                content: {
+                    error: e
+                }
+            });
+        })
+});
+
 /* GET releases for quotation listing. */
 router.get('/quotation/:id', (req, res, next) => {
     const client = new Client();
