b4ef734076c2ed5300036a7315a3703b0e4cb6ff
Chathura Hettiarachchi <Chathura.h@fidenz.com>
2019-09-11 18:10:03 +0530

    - added api to support invoice sent amount

--- routes/payments.js
+++ routes/payments.js
@@ -142,5 +142,43 @@ router.get('/all', async (req, res, next) => {
     })
 });
 
+/* GET quotations*/
+router.get('/all/:start_date/to/:end_date', async (req, res, next) => {
+    await pool.connect((err, client, release) => {
+        if (err){
+            release();
+            console.log(err);
+            res.status(400).json({status: 0, message: 'Something went wrong', content: {error: err}});
+        } else {
+            const sql = "SELECT p.payment_id, p.invoice_date, p.amount, p.sent_to_client, q.quotation_id, q.created_at AS quotation_created_at,q.title AS quotation_title, cs.name AS client_name, cs.code as client_code FROM payment_plans p INNER JOIN quotations q ON q.quotation_id=p.quotation_id INNER JOIN clients cs ON cs.client_id=q.client_id WHERE p.invoice_date BETWEEN $1 AND $2";
+            const params = [
+                req.params.start_date,
+                req.params.end_date,
+            ];
+            client.query(sql, params,(err2, result2) => {
+                release();
+                if (err2){
+                    console.log(err2);
+                    res.status(400).json({status: 0, message: 'Something went wrong', content: {error: err2}});
+                } else {
+                    if (result2.rows.length === 0) {
+                        res.status(200).json({
+                            status: 1,
+                            message: 'No payments found'
+                        });
+                    } else {
+                        res.status(200).json({
+                            status: 1,
+                            message: 'Available payments',
+                            content: {
+                                payments: result2.rows
+                            }
+                        });
+                    }
+                }
+            })
+        }
+    })
+});
 
 module.exports = router;
